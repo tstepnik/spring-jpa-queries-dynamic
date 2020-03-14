6e6453295b40dc08013132be74b71a2d733f709d
tstepnik <tomasz.stepnik77@gmail.com>
2020-03-14 09:36:29 +0100

    added essential files like gitignore

---
+++ src/main/java/pl/javastart/springjpaqueriesdynamic/SpringJpaQueriesDynamicApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart.springjpaqueriesdynamic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaQueriesDynamicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaQueriesDynamicApplication.class, args);
+    }
+
+}
